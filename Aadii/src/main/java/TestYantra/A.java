ab4da33bddaf1cea04d69e628e075c834a3a9763
DineshJhariya <dineshjhariya94@gmail.com>
2025-03-05 17:27:08 -0500

    pushed

---
+++ Aadii/src/main/java/TestYantra/A.java
@@ -0,0 +1,11 @@
+package TestYantra;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		System.out.println("hii");
+	}
+
+}
---
+++ Aadii/src/main/java/TestYantra/new_line.java
@@ -0,0 +1,12 @@
+package TestYantra;
+
+public class new_line {
+
+	
+	public static void main(String[] args) {
+		//WebDriver driver = new ChromeDriver();
+		//driver.get("https://www.cricbuzz.com/");
+	System.out.println("done");
+	}
+	
+}
